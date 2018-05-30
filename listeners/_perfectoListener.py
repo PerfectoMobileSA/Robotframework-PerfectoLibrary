d85d6bc72b45e7418d8399a7d8e730aec969989c
Deng, Jack <JACK.DENG@bmo.com>
2018-05-30 19:41:07 -0400

    1. added seleniu2Library support 2. Allow report test fail when step is failing to avoid RF teardown procedure kicks in before the test_end is called

--- listeners/_perfectoListener.py
+++ listeners/_perfectoListener.py
@@ -1,8 +1,11 @@
 import os
 import robot
 import inspect
+import pdb
+import sys
 from perfecto import *
 from SeleniumLibrary import SeleniumLibrary
+from Selenium2Library import Selenium2Library
 from AppiumLibrary import AppiumLibrary
 from robot.libraries.BuiltIn import BuiltIn
 from selenium.webdriver.common.actions.interaction import NONE
@@ -11,58 +14,75 @@ class _PerfectoListener(object):
     ROBOT_LISTENER_API_VERSION = 2
     ROBOT_LIBRARY_SCOPE = "GLOBAL"
     driver=''
-    
+
     def __init__(self):
+        # pdb.Pdb(stdout=sys.__stdout__).set_trace()
         self.ROBOT_LIBRARY_LISTENER = self
         self.bi=BuiltIn()
         self.reporting_client = NONE
         self.active=False
-    
+        self.stop_reporting = False
+
     def _start_test(self, name, attrs):
+        # pdb.Pdb(stdout=sys.__stdout__).set_trace()
         self.id=attrs['id']
         self.longname=attrs['longname']
         self.tags=attrs['tags']
         if not self.active:
             self._get_execontext()
-        if self.active:
-            self.execontext = PerfectoExecutionContext(self.driver, [','.join(attrs['tags'])], Job(attrs['longname'], 45), Project('Robotframework Test Project' + attrs['id'], '1.0'))
-            self.reporting_client = PerfectoReportiumClient(self.execontext)
+        if self.active and self.reporting_client!=NONE:
             self.reporting_client.test_start(name, TestContext(','.join(attrs['tags'])))
-            
+
     def _start_keyword(self, name, attrs):
         if not self.active:
             self._get_execontext()
-        if self.active and self.reporting_client==NONE:    
-            self.execontext = PerfectoExecutionContext(self.driver, [','.join(self.tags)], Job(self.longname, 45), Project('Robotframework Test Project' + self.id, '1.0'))
+        if self.active and self.reporting_client==NONE and self.stop_reporting!=True:
+            self.bi.log_to_console("creating reporting client")
+            self.execontext = PerfectoExecutionContext(self.driver, [','.join(self.tags)], Job(self.longname, '1'), Project('Robotframework Test Project ' + self.id, '1.0'))
             self.reporting_client = PerfectoReportiumClient(self.execontext)
-            self.reporting_client.step_start(attrs['kwname'])
-        elif self.active and attrs['kwname'].lower!="commtent":
-            self.reporting_client.step_start(attrs['kwname'])
+            self.reporting_client.test_start(self.longname, TestContext(','.join(self.tags)))
+
+        if self.active and "comment" not in attrs['kwname'].lower() and self.reporting_client!=NONE:
+            self.reporting_client.step_start(attrs['kwname']+ ' '+' '.join(attrs['args']))
+
+    def _end_keyword(self, name, attrs):
+        try:
+            if self.active and "comment" not in attrs['kwname'].lower() and self.reporting_client!=NONE and self.stop_reporting!=True:
+                self.reporting_client.step_end(attrs['kwname'] + ' ' + ' '.join(attrs['args']))
+                if attrs['status']!="PASS":
+                    self.reporting_client.test_stop(TestResultFactory.create_failure("Step Failed!! "+attrs['kwname'] + ' ' + ' '.join(attrs['args'])))
+                    self.stop_reporting=True
+        except Exception as e:
+            self.bi.log_to_console(e)
+
     def _get_execontext(self):
-#         self.bi.log_to_console("_get_execontext")
+        # self.bi.log_to_console("_get_execontext")
         try:
             aplib = self.bi.get_library_instance('AppiumLibrary')
-#             self.bi.log_to_console(aplib)
+            self.driver = aplib._current_application()
+            # self.bi.log_to_console(aplib)
+            self.active = True
         except:
-            aplib = self.bi.get_library_instance('SeleniumLibrary')
-#             self.bi.log_to_console(aplib)
-        try:
-            if isinstance(aplib,SeleniumLibrary):
-                self.driver = aplib.driver
-            elif isinstance(aplib,AppiumLibrary):
-                self.driver = aplib._current_application()
-            self.active=1
-        except Exception as e:
-#             self.bi.log_to_console(e)
-            self.active=False 
-            
-    def _stop_suite(self):
-        if self.active:
             try:
-                self.reporting_client.test_stop(TestResultFactory.create_success())
-            except Exception as e:
-                self.reporting_client.test_stop(TestResultFactory.create_failure(str(e)))
-    
-    def _stop_test(self, name, attrs):
-        if self.active:
-            self.reporting_client.step_end()
+                aplib = self.bi.get_library_instance('SeleniumLibrary')
+                self.driver = aplib.driver
+                # self.bi.log_to_console(aplib)
+                self.active = True
+            except:
+                try:
+                    aplib = self.bi.get_library_instance('Selenium2Library')
+                    self.driver = self.driver = aplib._current_browser()
+                    self.active = True
+                except:
+                    self.active = False
+
+    def _end_test(self, name, attrs):
+        try:
+            if self.active:
+                # self.bi.log_to_console("_end_test")
+                if attrs['status']=="PASS":
+                    self.reporting_client.test_stop(TestResultFactory.create_success())
+                else:
+                    self.reporting_client.test_stop(TestResultFactory.create_failure(attrs['message']))
+        except Exception as e:
+            self.bi.log_to_console(e)
