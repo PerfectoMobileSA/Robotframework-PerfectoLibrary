f4517e9effef2be549b8305b4741e11062f4683f
jackdperfecto <jackd@perfectomobile.com>
2019-03-30 11:39:53 -0400

    updated the version to 0.4.1 added download_summary_pdf_report to allow user to download summary report by using jobName, jobNumber as well we the tags

--- PerfectoLibrary/keywords/_devices.py
+++ PerfectoLibrary/keywords/_devices.py
@@ -4,7 +4,9 @@ import robot
 import inspect
 import PerfectoLibrary
 import appium
+import urllib2
 import time
+import sys
 from perfecto import *
 from robot.libraries.BuiltIn import BuiltIn
 # from appium import webdriver
@@ -15,6 +17,10 @@ from ..listeners import *
 class _DeviceKeywords(KeywordGroup):
     def __init__(self):
         self.bi = BuiltIn()
+        self.reportPdfUrl = ''
+
+    def init_driver(self):
+        self._check_driver()
 
     def _check_driver(self):
         # self.bi.log_to_console("_check_driver")
@@ -22,6 +28,7 @@ class _DeviceKeywords(KeywordGroup):
         try:
             aplib = self.bi.get_library_instance('AppiumLibrary')
             self.driver = aplib._current_application()
+            self.reportPdfUrl = self.driver.capabilities['reportPdfUrl']
             return True
         except:
             self.bi.log_to_console(
@@ -316,4 +323,61 @@ class _DeviceKeywords(KeywordGroup):
                 time.sleep(60)
                 self.device_info('model')
             return True
+        return False
+
+    def download_summary_pdf_report(self, reportpath, sectoken, executionID=None, jobName=None, jobNumber=None,
+                                    tags=None):
+        '''
+
+        :param reportpath: the local path where you want to store the pdf reports
+                sectoken: perfecto securitytoken to access the report
+                executionID: the executionID of the reports
+                jobName: the jobName of the reports
+                jobID: the jobID of the reports
+                tags: the tags of the reports
+        :return: return false if anything go wrong
+        '''
+        exeRptUrl = self.reportPdfUrl
+        rptQuery = ''
+        if self.reportPdfUrl != '':
+
+            if executionID == None and jobName == None and jobNumber == None and tags == None:
+                exeRptUrl = self.reportPdfUrl
+            else:
+                if executionID != None:
+                    if rptQuery == '':
+                        rptQuery = 'externalId[0]=' + executionID
+                    else:
+                        ptQuery = ptQuery + '&' + 'externalId[0]=' + executionID
+                if jobName != None:
+                    if rptQuery == '':
+                        rptQuery = 'jobName[0]=' + jobName
+                    else:
+                        rptQuery = rptQuery + '&' + 'jobName[0]=' + jobName
+                if tags != None:
+                    if rptQuery == '':
+                        rptQuery = 'tags[0]=' + tags
+                    else:
+                        rptQuery = rptQuery + '&' + 'tags[0]=' + tags
+                if jobNumber != None:
+                    if rptQuery == '':
+                        rptQuery = 'jobNumber[0]=' + jobNumber
+                    else:
+                        rptQuery = rptQuery + '&' + 'jobNumber[0]=' + jobNumber
+                exeRptUrl = self.reportPdfUrl.split('pdf?')[0] + 'pdf?' + rptQuery
+                # self.bi.log_to_console(exeRptUrl)
+
+            time.sleep(10)  # have to sleep for 10 seconds
+            try:
+                headers = {'PERFECTO-AUTHORIZATION': sectoken, }
+                req = urllib2.Request(exeRptUrl, None, headers)
+                rp = urllib2.urlopen(req)
+                with open(reportpath + self.reportPdfUrl.rsplit('=', 1)[-1] + '.pdf', 'wb') as output:
+                    output.write(rp.read())
+                    output.close()
+                return True
+            except:
+                self.bi.log_to_console(traceback.print_exc())
+                return False
+        self.bi.log_to_console("empty with " + self.reportPdfUrl)
         return False
